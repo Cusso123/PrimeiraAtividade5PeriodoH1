e134abc6f0838a8d906b57ca1432746620a2433d
MarcusCosta <marcu@DESKTOP-LPOVEDR>
2024-03-24 21:49:25 -0300

    first commit

--- src/components/Ola/Ola.tsx
+++ src/components/Ola/Ola.tsx
@@ -1,43 +1,49 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 
-interface OlaProps {
-    nomeProps:string
+interface OlaExe {
+  nomeExe: string;
 }
 
-const Ola : React.FC<OlaProps> = ({nomeProps}) => {
+const Ola: React.FC<OlaExe> = ({ nomeExe }) => {
+  const [contadorLikes, setContadorLikes] = useState<number>(0);
 
-    const [contadorLikes, setContadorLikes] = useState<number>(0);
+  const incrementarContador = () => setContadorLikes(contadorLikes + 1);
+  const diminuirContador = () => setContadorLikes(contadorLikes - 1);
 
-    const incrementarContador = () =>  setContadorLikes(contadorLikes + 1);
-
-    return (
+  return (
     <View style={styles.container}>
-          <Text style={styles.greeting}>
-            Olá {nomeProps}
-            seu total de é Likes {contadorLikes}
-            </Text>
+      <Text style={styles.greeting}>
+        Olá {nomeExe}, seu total de Likes é {contadorLikes}
+      </Text>
 
-        <Button
-          title="Like"         
-          onPress={incrementarContador}
-          color="blue"
-        />
+      <View style={styles.buttonContainer}>
+        <Button title="Like" onPress={incrementarContador} color="green" />
+        <View style={{ width: 10 }} />
+        <Button title="Dislike" onPress={diminuirContador} color="red" />
+      </View>
     </View>
-    );
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    greeting: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      margin: 16,
-    },
-  });
-  
-  export default Ola;
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+  greeting: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 16,
+    color: 'white',
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+});
+
+export default Ola;
